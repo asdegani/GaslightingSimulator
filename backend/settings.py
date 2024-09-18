8621b1459c863b11508ce898812d9644d50804fa
asdegani <asdegani@microsoft.com>
2024-09-18 15:49:45 +0300

    Some RTL and Hebrew strings (#3)

--- backend/settings.py
+++ backend/settings.py
@@ -44,8 +44,8 @@ class _UiSettings(BaseSettings):
     title: str = "Contoso"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "סימולטור גזלייטינג"
+    chat_description: str = "כיצד להתמודד עם מניפולציות רגשיות"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
